953a3eb78fe4c1f1adc88f9371d81cf4d026c60c
Anthony <Frenchy2199@yahoo.com>
2020-01-29 13:13:30 -0700

    -Added signals for the buttons -Moving fighting mechanic to the fightHUD -Added InfoLabel to show messages on the screen -Updated the show and hide function -Added a display_msg_timer function -Added DamageTakenLabel -Added show_and_scroll_damage_taken function

--- Scenes_Scripts/FightHUD.gd
+++ Scenes_Scripts/FightHUD.gd
@@ -1,8 +1,9 @@
 extends CanvasLayer
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal hit
+signal run
+signal inventory
+signal fight_over
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,6 +20,7 @@ func show():
 	$HealthLabel.show()
 	$HealthNumberLabel.text = str(Globals.player_health)
 	$HealthNumberLabel.show()
+	$InfoLabel.show()
 
 
 func hide():
@@ -27,7 +29,45 @@ func hide():
 	$RunButton.hide()
 	$HealthLabel.hide()
 	$HealthNumberLabel.hide()
+	$InfoLabel.hide()
+
+#Displays a message for a certain amount of time and then goes away
+func display_msg_timer(message, time):
+	$InfoLabel.text = message
+	$InfoLabel.show()
+	yield(get_tree().create_timer(time), "timeout")
+	$InfoLabel.hide()
+
+
+func show_and_scroll_damage_taken(damage):
+	$DamageTakenLabel.text = str(damage)
+	$DamageTakenLabel.show()
+	var original_pos = $DamageTakenLabel.margin_top
+	while $DamageTakenLabel.margin_top > 0:
+		$DamageTakenLabel.margin_top -= 1
+		yield(get_tree().create_timer(0.025), "timeout")
+		$DamageTakenLabel.show()
+	$DamageTakenLabel.hide()
+	$DamageTakenLabel.margin_top = original_pos
 
 
 func spawn_enemy(enemy):
 	get_tree().get_root().add_child(enemy)
+
+
+func _on_HitButton_pressed():
+	Globals.currentMonster.defend(Globals.player_attack())
+	var damage_monster = Globals.currentMonster.attack()
+	Globals.player_defend(damage_monster)
+	show_and_scroll_damage_taken(damage_monster)
+	$HealthNumberLabel.text = str(Globals.player_health)
+	$HealthNumberLabel.show()
+	emit_signal("hit")
+
+
+func _on_InventoryButton_pressed():
+	emit_signal("inventory")
+
+
+func _on_RunButton_pressed():
+	emit_signal("run")
