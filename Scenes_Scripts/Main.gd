a5977152a86df2e2ed43501d97f247963820613c
Anthony <Frenchy2199@yahoo.com>
2020-02-20 13:09:47 -0700

    -Removed fightingHUD(old hud) -Removed the encounterMObHUD_run function -updated the ongoing fight variable to true when a fight is happening, and flase when it's not -Removed a function that waited for an encounterMobHUD signal, since that scene no longer exists

--- Scenes_Scripts/Main.gd
+++ Scenes_Scripts/Main.gd
@@ -9,7 +9,6 @@ func _ready():
 	randomize()	# set the seed for random numbers
 	$EncounterMobHUD.hide()
 	$InventoryHUD.hide()
-	$FightingHUD.hide()
 	$FightHUD.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -39,10 +38,10 @@ func _on_DefaultHUD_next_room():
 		# Show EncounterHUD
 		$DefaultHUD.hide()
 		Globals.currentMonster = room_node
+		Globals.is_fight_ongoing = true
 		$FightHUD.show()
 		$FightHUD.display_msg_timer(room_node.encounter_message, 2)
-		yield($FightHUD, "fight_over")	# we wait until the fight is over
-		# At this point the user can only emit two signals, fight and run which are taken care with handler functions listening
+		# At this point the fight is on going and when it's over, a signal will be emited and taken care of in another function
 	elif room_node.encounter_type == "Reward":
 		room_node.roll_reward()
 		$DefaultHUD.show()	# this updates our gold label for the HUD
@@ -50,50 +49,6 @@ func _on_DefaultHUD_next_room():
 		#Hide our current hude
 		$DefaultHUD.hide()
 		room_node.connect("leave", self, "leave_merchant_room")
-
-
-func _on_EncounterMobHUD_fight():
-	$EncounterMobHUD.hide()
-	Globals.currentMonster = room_node
-	$FightHUD.show()
-	
-	#Globals.fight1(room_node)
-	#Globals.fight(room_node)
-	yield(Globals, "fight_over")	# wait for the fight to be over before we proceed
-	if  Globals.player_health > 0:
-		# Fight was successfull and we restore defaultHUD, and remove the monster from the screen
-		$DefaultHUD.show()
-		room_node.queue_free()
-	else:
-		# The player has died, switch to gameover screen
-		pass
-
-
-func _on_EncounterMobHUD_run():
-	# chance of escape is a number between 0 and 100
-	if Globals.attempt_to_run(room_node.chance_of_escape) == true:
-		$EncounterMobHUD/RoomInformationLabel.text = "You've managed to escape!"
-		$EncounterMobHUD/RoomInformationLabel.show()
-		var timer = Timer.new()
-		get_tree().get_root().add_child(timer)
-		timer.connect("timeout", self, "switch_to_defaultHUD", [$EncounterMobHUD, timer])
-		timer.wait_time = 2
-		timer.start()
-		# Remove the buttons so that the user doens't try to break my game
-		$EncounterMobHUD/FightButton.hide()
-		$EncounterMobHUD/RunButton.hide()
-	else:
-		# User did not manage to escape and must now fight, switch to FightingHUD
-		$EncounterMobHUD.hide()
-		Globals.fight(room_node)
-		yield(Globals, "fight_over")	# wait for the fight to be over before we proceed
-		if Globals.player_health > 0:
-			# Fight was successfull and we restore defaultHUD, and remove the monster from the screen
-			$DefaultHUD.show()
-			room_node.queue_free()
-		else:
-			# The player has died, switch to gameover screen
-			pass
 
 
 func switch_to_defaultHUD(currentHud, timer):
@@ -122,3 +77,14 @@ func leave_merchant_room():
 	room_node.queue_free()	# remove the merchant node from our main scene
 	$DefaultHUD.show()
 
+
+
+func _on_FightHUD_fight_over():
+	Globals.is_fight_ongoing = false
+	if  Globals.player_health > 0:
+		# Fight was successfull and we restore defaultHUD, and remove the monster from the screen
+		$DefaultHUD.show()
+		room_node.queue_free()
+	else:
+		# The player has died, switch to gameover screen
+		pass
