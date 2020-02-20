938c29ccbd28d60a0ffccf577bf3ab7bcfeb54de
Anthony <Frenchy2199@yahoo.com>
2020-02-20 13:08:53 -0700

    -Added a delay in the fight -Hid the button while the player is hitting -Implemented the run away button -Added return to on_hitButton_pressed to fix a bug from the buttons coming back to screen after finishing a fight -Implemented the inventory button during fight -When inventory opens, the fight health labels are hidden now since they don't line up with the inventory health labels -Fixed an issue where player was no longer recieving loot from killing an enemy

--- Scenes_Scripts/FightHUD.gd
+++ Scenes_Scripts/FightHUD.gd
@@ -1,17 +1,15 @@
 extends CanvasLayer
 
-signal hit
-signal run
-signal inventory
 signal fight_over
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$InventoryHUD.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+#	if Globals.currentMonster.health <= 0:
+#		emit_signal("fight_over")
 
 func show():
 	$HitButton.show()
@@ -30,6 +28,22 @@ func hide():
 	$HealthLabel.hide()
 	$HealthNumberLabel.hide()
 	$InfoLabel.hide()
+
+
+
+# Function to hide all buttons because we don't want the user to interact with the scene while something is happening
+func hide_buttons():
+	$HitButton.hide()
+	$InventoryButton.hide()
+	$RunButton.hide()
+
+
+# Function to show all buttons when we want to allow the user to interact with the scene again
+func show_buttons():
+	$HitButton.show()
+	$InventoryButton.show()
+	$RunButton.show()
+
 
 #Displays a message for a certain amount of time and then goes away
 func display_msg_timer(message, time):
@@ -56,18 +70,84 @@ func spawn_enemy(enemy):
 
 
 func _on_HitButton_pressed():
+	hide_buttons()
+	yield(get_tree().create_timer(0.5), "timeout")
 	Globals.currentMonster.defend(Globals.player_attack())
+	if Globals.currentMonster.health <= 0:
+		hide()
+		# Enemy is dead, player recieves loot
+		Globals.currentMonster.roll_for_loot()
+		yield(get_tree().create_timer(1.5), "timeout")
+		emit_signal("fight_over")
+		return
 	var damage_monster = Globals.currentMonster.attack()
+	yield(get_tree().create_timer(1.5), "timeout")
 	Globals.player_defend(damage_monster)
 	show_and_scroll_damage_taken(damage_monster)
 	$HealthNumberLabel.text = str(Globals.player_health)
 	$HealthNumberLabel.show()
-	emit_signal("hit")
+	if Globals.player_health <= 0:
+		hide()
+		emit_signal("fight_over")
+	else:
+		show_buttons()
 
 
 func _on_InventoryButton_pressed():
-	emit_signal("inventory")
+	hide_buttons()
+	# This is because the inventory has it's own health label and the fight and inventory labels don't line up
+	$HealthLabel.hide()
+	$HealthNumberLabel.hide()
+	$InventoryHUD.update()
+	$InventoryHUD.show()
+	var size_of_inventory = Globals.player_inventory.size()
+	yield($InventoryHUD, "back")
+	$InventoryHUD.hide()
+	if Globals.player_inventory.size() < size_of_inventory:
+		# inventory was used, enemy attacks
+		$HealthLabel.show()
+		$HealthNumberLabel.text = Globals.player_health
+		$HealthNumberLabel.show()
+		var damage_monster = Globals.currentMonster.attack()
+		yield(get_tree().create_timer(1.5), "timeout")
+		Globals.player_defend(damage_monster)
+		show_and_scroll_damage_taken(damage_monster)
+		$HealthNumberLabel.text = str(Globals.player_health)
+		$HealthNumberLabel.show()
+		if Globals.player_health <= 0:
+			hide()
+			emit_signal("fight_over")
+		else:
+			show_buttons()
+	else:
+		show_buttons()
+		$HealthLabel.show()
+		$HealthNumberLabel.show()
 
 
 func _on_RunButton_pressed():
-	emit_signal("run")
+	# chance of escape is a number between 0 and 100
+	hide_buttons()
+	if randi() % 100 < Globals.currentMonster.chance_of_escape:
+		# user managed to escape
+		$InfoLabel.text = "You managed to escape!"
+		$InfoLabel.show()
+		yield(get_tree().create_timer(1.5), "timeout")
+		hide()
+		emit_signal("fight_over")
+	else:
+		# User did not manage to escape, monsters use their turn now
+		$InfoLabel.text = "Failed to escape!"
+		$InfoLabel.show()
+		var damage_monster = Globals.currentMonster.attack()
+		yield(get_tree().create_timer(1.5), "timeout")
+		$InfoLabel.hide()
+		Globals.player_defend(damage_monster)
+		show_and_scroll_damage_taken(damage_monster)
+		$HealthNumberLabel.text = str(Globals.player_health)
+		$HealthNumberLabel.show()
+		if Globals.player_health <= 0:
+			hide()
+			emit_signal("fight_over")
+		else:
+			show_buttons()
